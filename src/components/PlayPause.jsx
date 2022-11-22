7242ee18b8a3fdf4e5b14417f97e1cb2854a6815
sugarmouse <tanghao19950911@gmail.com>
2022-11-22 21:36:24 +0800

    implement play and pause functionallity

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,25 +1,21 @@
 import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
 
-const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => {
-  console.log('hah');
-
-  return (
-    isPlaying && activeSong?.title === song.title
-      ? (
-        <FaPauseCircle
-          onClick={handlePause}
-          size={35}
-          className="text-gray-300"
-        />
-      )
-      : (
-        <FaPlayCircle
-          onClick={handlePlay}
-          size={35}
-          className="text-gray-300"
-        />
-      )
-  );
-};
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => (
+  isPlaying && activeSong?.title === song.title
+    ? (
+      <FaPauseCircle
+        onClick={handlePause}
+        size={35}
+        className="text-gray-300"
+      />
+    )
+    : (
+      <FaPlayCircle
+        onClick={handlePlay}
+        size={35}
+        className="text-gray-300"
+      />
+    )
+);
 
 export default PlayPause;
