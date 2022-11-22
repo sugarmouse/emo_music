6c294d455c31c78ee6cfc9b64571e7d0596866fa
sugarmouse <tanghao19950911@gmail.com>
2022-11-22 19:04:36 +0800

    fetch data from shazamCore API

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('x-rapidapi-key', '533f9e161bmsh0e5f16f78c75b1dp19d928jsn543ac38269dd');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
