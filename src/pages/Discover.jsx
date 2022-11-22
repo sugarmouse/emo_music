98ae66537f5932274a63e38dfb8e8cb410269402
sugarmouse <tanghao19950911@gmail.com>
2022-11-22 19:07:40 +0800

    change mock data to real data

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,13 @@
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
-  console.log(genres);
+  const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitile = 'Pop';
+
+  if (isFetching) return <Loader title="Loading songs" />;
+  if (error) return <Error />;
 
   return (
     <div className="flex flex-col">
@@ -20,9 +24,9 @@ const Discover = () => {
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data?.map((song, i) => (
           <SongCard
-            key={song}
+            key={song.key}
             song={song}
             i={i}
           />
