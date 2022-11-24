99da1846b0014cf94e1f4cc6e35b407409b02a56
sugarmouse <tanghao19950911@gmail.com>
2022-11-24 14:37:49 +0800

    implementing fetching song details

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -6,15 +6,18 @@ export const shazamCoreApi = createApi({
     baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
     prepareHeaders: (headers) => {
       headers.set('x-rapidapi-key', '533f9e161bmsh0e5f16f78c75b1dp19d928jsn543ac38269dd');
-
       return headers;
     },
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+    }),
   }),
 });
 
 export const {
   useGetTopChartsQuery,
+  useGetSongDetailsQuery,
 } = shazamCoreApi;
